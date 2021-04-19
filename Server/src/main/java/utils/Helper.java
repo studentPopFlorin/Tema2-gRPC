fc690f311449e0496ec470272cd82c3175667abc
studentPopFlorin <78866511+studentPopFlorin@users.noreply.github.com>
2021-04-19 20:37:16 +0300

    Implementation in Helper class.
    Implemented method to return zodiac for given birth date.

--- Server/src/main/java/utils/Helper.java
+++ Server/src/main/java/utils/Helper.java
@@ -23,4 +23,27 @@ public class Helper {
             System.out.println("File not found!");
         }
     }
+
+    public String returnZodiacForBirthDate(String birthDate, List<ZodiacDetails> zodiacArray){
+        int birthDateDay=Integer.parseInt(birthDate.split("/")[1]);
+        int birthDateMonth=Integer.parseInt(birthDate.split("/")[0]);
+        for(int index=0;index<zodiacArray.size();index++) {
+
+            int zodiacStartDateDay = Integer.parseInt(zodiacArray.get(index).getStartDate().split("/")[1]);
+            int zodiacStartDateMonth = Integer.parseInt(zodiacArray.get(index).getStartDate().split("/")[0]);
+            int zodiacEndDateDay = Integer.parseInt(zodiacArray.get(index).getEndDate().split("/")[1]);
+            int zodiacEndDateMonth = Integer.parseInt(zodiacArray.get(index).getEndDate().split("/")[0]);
+
+            if(birthDateMonth>zodiacStartDateMonth && birthDateMonth<zodiacEndDateMonth)
+            {
+                return zodiacArray.get(index).getName();
+            }else if(birthDateMonth==zodiacStartDateMonth && birthDateDay>=zodiacStartDateDay)
+            {
+                return zodiacArray.get(index).getName();
+            } else if(birthDateMonth==zodiacEndDateMonth && birthDateDay<=zodiacEndDateDay){
+                return zodiacArray.get(index).getName();
+            }
+        }
+        return "";
+    }
 }
