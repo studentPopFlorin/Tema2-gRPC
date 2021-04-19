b4f0bcee7918ad1b400f4f1c83c7cce06ac42e16
studentPopFlorin <78866511+studentPopFlorin@users.noreply.github.com>
2021-04-19 20:24:34 +0300

    Added and implemented ZodiacDetails class.

---
+++ Server/src/main/java/utils/ZodiacDetails.java
@@ -0,0 +1,23 @@
+package utils;
+
+public class ZodiacDetails {
+    private String name;
+    private String startDate;
+    private String endDate;
+    
+    public ZodiacDetails(String name, String startDate, String endDate){
+        this.name=name;
+        this.startDate=startDate;
+        this.endDate=endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getStartDate() {
+        return startDate;
+    }
+    public String getEndDate() {
+        return endDate;
+    }
+}
